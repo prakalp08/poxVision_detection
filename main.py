572e58dff81dd44e70bc2834965f8927ec8c45d6
AarizZafar <aariz.zafar01@gmail.com>
2023-11-10 22:39:20 +0530

    modular coding for base model preparation has been created

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from poxVisionDetection import logging,CustomException
 from poxVisionDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from poxVisionDetection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "DATA INGESTION STAGE"
 
@@ -12,3 +13,14 @@ try:
 except Exception as e:
     logging.exception(e)
     CustomException(e,sys)
+
+
+STAGE_NAME = 'PREPARE BASE MODEL'
+
+try:
+    logging.info(f'\n\n >>>>>>>>>>>>>>>>>>>>>>>> {STAGE_NAME} <<<<<<<<<<<<<<<<<<<<<<<<<<< \n')
+    PBMobj = PrepareBaseModelTrainingPipeline()
+    PBMobj.main()
+    logging.info(f'\n >>>>>>>>>>>>> {STAGE_NAME} >>>>>>> [COMPLETED] <<<<<<<<<<<<<<<<<<<\n\nX===============================================================================X')
+except Exception as e:
+    logging.exception(CustomException(e,sys))
--- src/poxVisionDetection/entity/config_entity.py
+++ src/poxVisionDetection/entity/config_entity.py
@@ -3,7 +3,18 @@ from dataclasses import dataclass
 
 @dataclass(frozen = True)
 class DataIngestionConfig: # BELOW ARE THE RETURN TYPES 
-    root_dir                : Path
-    source_url              : str
-    local_data_file         : Path
-    unzip_dir               : Path
+    root_dir                   : Path
+    source_url                 : str
+    local_data_file            : Path
+    unzip_dir                  : Path
+
+@dataclass(frozen = True)
+class PrepareBaseModelConfig:
+    root_dir                   : Path 
+    base_model_path            : Path
+    updated_base_model_path    : Path
+    params_image_size          : list
+    params_learning_rate       : float 
+    params_include_top         : bool
+    params_weight              : str
+    params_classes             : int
